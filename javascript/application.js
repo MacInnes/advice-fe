a7b78b178694c88f1e2b18848c6e7ec3e9111e27
MacInnes <califormula@gmail.com>
2019-01-08 18:27:12 -0700

    basic subject search working

--- javascript/application.js
+++ javascript/application.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  
+
+  $('#subject-submit').click(function(){
+    var subject = $('#subject').val();
+    fetch(`https://api.adviceslip.com/advice/search/${subject}`)
+      .then(response => response.json())
+      .then(json => {
+        var advice = json.slips[Math.floor(Math.random() * json.slips.length)]
+        console.log(advice.advice);
+        $('#subject-results').text(`${advice.advice}`);
+      })
+  })
+
+
+
 })
